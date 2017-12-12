8896c357bef92f394d9a204d19a37b634b11f3c9
josephdlawson21 <josephdlawson21@gmail.com>
2017-12-12 16:40:31 -0500

    working version

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
-
+require "pry"
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
-show_character_movies(character)
+while character != "exit"
+  show_character_movies(character)
+  character = get_character_from_user
+end
+leave
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,11 +2,46 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+
+def find_character_movies(get_character_movies_from_api,character)
+  movie_array = []
+  while movie_array.length == 0
+    if get_character_movies_from_api['next'] != nil
+      get_character_movies_from_api["results"].each do |character_name|
+        if character == character_name["name"].downcase
+          movie_array = character_name["films"]
+        end
+      end
+      # binding.pry
+      get_character_movies_from_api = get_character_movies_from_api(character, get_character_movies_from_api["next"])
+      # binding.pry
+    else
+      return puts "invalid entry"
+    end
+  end
+  movie_array
+end
+
+def get_movie_array(find_character_movies)
+  result = []
+  find_character_movies ||= []
+  find_character_movies.each do |movie_link|
+    hold = RestClient.get(movie_link)
+    result << JSON.parse(hold)
+  end
+  result
+end
+
+def leave
+  puts "Thanks!"
+end
+
+
+def get_character_movies_from_api(character, page = 'http://www.swapi.co/api/people/')
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  all_characters = RestClient.get(page)
   character_hash = JSON.parse(all_characters)
-  
+  # binding.pry
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -19,11 +54,16 @@ def get_character_movies_from_api(character)
 end
 
 def parse_character_movies(films_hash)
+  films_hash.each do |film|
+    puts film["title"]
+  end
   # some iteration magic and puts out the movies in a nice list
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
+  films_hash = find_character_movies(films_hash,character)
+  films_hash = get_movie_array(films_hash)
   parse_character_movies(films_hash)
 end
 
